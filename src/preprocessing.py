712a015f96eaeeb5e248594803209b7016691b90
KeerthanaUSD <kkr@sandiego.edu>
2025-10-20 12:40:22 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import os
+
+# Dummy data creation
+os.makedirs("/opt/ml/processing/output", exist_ok=True)
+df = pd.DataFrame({
+    "feature1": [1,2,3,4,5],
+    "feature2": [10,20,30,40,50],
+    "label": [0,1,0,1,0]
+})
+
+# Save dummy train data
+df.to_csv("/opt/ml/processing/output/train.csv", index=False)
+print("✅ Preprocessing complete. Dummy data saved.")
 
