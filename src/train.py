3a1df121be90cd8b6b184a4fb85f537679d83ae2
KeerthanaUSD <kkr@sandiego.edu>
2025-10-20 12:40:43 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,21 @@
 
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib
+import os
+
+# Read dummy data
+train_path = "/opt/ml/input/data/train/train.csv"
+df = pd.read_csv(train_path)
+
+X = df[["feature1","feature2"]]
+y = df["label"]
+
+# Train dummy model
+model = LogisticRegression()
+model.fit(X, y)
+
+# Save model
+os.makedirs("/opt/ml/model", exist_ok=True)
+joblib.dump(model, "/opt/ml/model/model.joblib")
+print("✅ Training complete. Dummy model saved.")
